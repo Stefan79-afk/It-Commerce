22ea8585b9be6667588b225767993f14a5bfc8a3
Stefan79-afk <epsilon.number79@gmail.com>
2026-05-28 23:24:55 +0300

    fix: product delete error

--- products-service/src/main/java/com/example/products_service/repositories/ProductImageRepository.java
+++ products-service/src/main/java/com/example/products_service/repositories/ProductImageRepository.java
@@ -10,6 +10,10 @@ import com.example.products_service.entities.ProductImage;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ProductImageRepository extends JpaRepository<ProductImage, UUID> {
 
@@ -21,5 +25,8 @@ public interface ProductImageRepository extends JpaRepository<ProductImage, UUID
 
     Optional<ProductImage> findByIdAndProductId(UUID id, UUID productId);
 
-    void deleteByProductId(UUID productId);
+    @Transactional
+    @Modifying
+    @Query("DELETE FROM ProductImage p WHERE p.productId = :productId")
+    void deleteByProductId(@Param("productId") UUID productId);
 }
